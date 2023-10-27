f05d2b5cf04b06f168d6fa99af1183c9d5b898f2
Camilo-Valencia <cavserna@gmail.com>
2023-10-26 19:36:53 -0500

    Iniciando repositorio

---
+++ generadorContrasena.py
@@ -0,0 +1,36 @@
+import string
+import random
+
+mayusculas = int(input("Ingrese el número de mayúsculas: "))
+minusculas= int(input("Ingrese el número de minúsculas: "))
+numeros= int(input("Ingrese la cantidad de números: "))
+longitud= int(input("Ingrese la longitud deseada: "))
+
+
+def generar_caracteres (mayusculas):
+    caracter = string.ascii_uppercase
+    cantidad_mayusculas = ""
+    for i in range(mayusculas):
+        cantidad_mayusculas += random.choice(caracter)
+        print(cantidad_mayusculas)
+    
+        
+def generar_minusculas(minusculas):
+    caracter = string.ascii_lowercase
+    cantidad_minusculas = ""
+    for j in range(minusculas):
+        cantidad_minusculas += random.choice(caracter)
+        print(cantidad_minusculas)
+
+def generar_numeros(numeros):
+    caracter= string.digits
+    cantidad_numeros= ""
+    for k in range(numeros):
+        cantidad_numeros += random.choice(caracter)
+        return cantidad_numeros
+        
+generar_numeros(numeros)
+
+
+
+
