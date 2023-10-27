d66c2fa8e39068d5e367441fd2c4cf56c8a81138
Camilo-Valencia <cavserna@gmail.com>
2023-10-27 18:21:14 -0500

    Ejercicio validador

---
+++ validador.py
@@ -0,0 +1,8 @@
+print ("Bienvenido")
+consultar_numero=int(input("Ingrese un número: "))
+
+if consultar_numero % 2 == 0:
+    print (consultar_numero, "es par")
+    
+else:
+    print (consultar_numero, "es impar")
